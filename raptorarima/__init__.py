326c3b5b91bc3b7ff8a9f40034d1f0bd34f17815
swaroop9ai9 <62091485+swaroop9ai9@users.noreply.github.com>
2020-08-22 21:45:37 +0530

    commit -1

---
+++ raptorarima/__init__.py
@@ -0,0 +1,159 @@
+name = "raptor_arima_api"
+
+
+
+import numpy as np
+import pandas as pd
+import random
+from movingaverage import ma_process
+from movingaverage import lag_view
+from autoregressive import ar_process
+from plots import plot_acf
+from plots import plot_pacf
+# import statsmodels.api as sm
+
+def least_squares(x, y):
+    return np.linalg.inv((x.T @ x)) @ (x.T @ y)
+
+class LinearModel:
+    def __init__(self, fit_intercept=True):
+        self.fit_intercept = fit_intercept
+        self.beta = None
+        self.intercept_ = None
+        self.coef_ = None
+
+    def _prepare_features(self, x):
+        if self.fit_intercept:
+            x = np.hstack((np.ones((x.shape[0], 1)), x))
+        return x
+
+    def fit(self, x, y):
+        x = self._prepare_features(x)
+        self.beta = least_squares(x, y)
+        if self.fit_intercept:
+            self.intercept_ = self.beta[0]
+            self.coef_ = self.beta[1:]
+        else:
+            self.coef_ = self.beta
+
+    def predict(self, x):
+        x = self._prepare_features(x)
+        return x @ self.beta
+
+    def fit_predict(self, x, y):
+        self.fit(x, y)
+        return self.predict(x)
+
+
+class ARIMA(LinearModel):
+    def __init__(self, q, d, p):
+        """
+        An ARIMA model.
+        :param q: (int) Order of the MA model.
+        :param p: (int) Order of the AR model.
+        :param d: (int) Number of times the data needs to be differenced.
+        """
+        super().__init__(True)
+        self.p = p
+        self.d = d
+        self.q = q
+        self.ar = None
+        self.resid = None
+
+    def prepare_features(self, x):
+        if self.d > 0:
+            x = difference(x, self.d)
+
+        ar_features = None
+        ma_features = None
+
+        # Determine the features and the epsilon terms for the MA process
+        if self.q > 0:
+            if self.ar is None:
+                self.ar = ARIMA(0, 0, self.p)
+                self.ar.fit_predict(x)
+            eps = self.ar.resid
+            eps[0] = 0
+
+            # prepend with zeros as there are no residuals_t-k in the first X_t
+            ma_features, _ = lag_view(np.r_[np.zeros(self.q), eps], self.q)
+
+        # Determine the features for the AR process
+        if self.p > 0:
+            # prepend with zeros as there are no X_t-k in the first X_t
+            ar_features = lag_view(np.r_[np.zeros(self.p), x], self.p)[0]
+
+        if ar_features is not None and ma_features is not None:
+            n = min(len(ar_features), len(ma_features))
+            ar_features = ar_features[:n]
+            ma_features = ma_features[:n]
+            features = np.hstack((ar_features, ma_features))
+        elif ma_features is not None:
+            n = len(ma_features)
+            features = ma_features[:n]
+        else:
+            n = len(ar_features)
+            features = ar_features[:n]
+
+        return features, x[:n]
+
+    def fit(self, x):
+        features, x = self.prepare_features(x)
+        super().fit(features, x)
+        return features
+
+    def fit_predict(self, x):
+        """
+        Fit and transform input
+        :param x: (array) with time series.
+        """
+        features = self.fit(x)
+        return self.predict(x, prepared=(features))
+
+    def predict(self, x, **kwargs):
+        """
+        :param x: (array)
+        :kwargs:
+            prepared: (tpl) containing the features, eps and x
+        """
+        features = kwargs.get('prepared', None)
+        if features is None:
+            features, x = self.prepare_features(x)
+
+        y = super().predict(features)
+        self.resid = x - y
+
+        return self.return_output(y)
+
+    def return_output(self, x):
+        if self.d > 0:
+            x = undo_difference(x, self.d)
+        return x
+
+    def forecast(self, x, n):
+        """
+        Forecast the time series.
+
+        :param x: (array) Current time steps.
+        :param n: (int) Number of time steps in the future.
+        """
+        features, x = self.prepare_features(x)
+        y = super().predict(features)
+
+        # Append n time steps as zeros. Because the epsilon terms are unknown
+        y = np.r_[y, np.zeros(n)]
+        for i in range(n):
+            feat = np.r_[y[-(self.p + n) + i: -n + i], np.zeros(self.q)]
+            y[x.shape[0] + i] = super().predict(feat[None, :])
+        return self.return_output(y)
+
+# q = 1
+# d = 0
+# p = 3
+#
+# m = ARIMA(q, d, p)
+# print(type(m))
+# data = sm.datasets.sunspots.load_pandas().data
+# x = data['SUNACTIVITY'].values.squeeze()
+# pred = m.fit_predict(x)
+# print(pred)
