ce0325ed91e9c8f7da7e85a4144ba858eeb81ee7
swaroop9ai9 <62091485+swaroop9ai9@users.noreply.github.com>
2020-08-22 21:42:53 +0530

    Commit -1

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md","r") as fh:
+	long_description = fh.read()
+INSTALL_REQUIRES = (
+    ['pandas', 'requests']
+)
+
+
+setuptools.setup(
+    name="raptor_arima_api",
+    version="0.0.1",
+    author="M V D SATYA SWAROOP",
+    author_email="swaroop9ai9@gmail.com",
+    description="Implementation of ARIMA model (Auto Regressive Integrated Moving Average)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/swaroop9ai9/Fintech/raptor_arima_api",
+    packages=setuptools.find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
